70d579251e218d554dc1a431605cf174f30927ba
Jonas Attrup <attrup.jonas@gmail.com>
2025-09-08 15:25:11 +0200

    Moved derive macro functionality out of lib

--- fairjax/src/lib.rs
+++ fairjax/src/lib.rs
@@ -1,17 +1,10 @@
-extern crate proc_macro;
 use proc_macro::TokenStream;
-
-use quote::quote;
 use syn::{DeriveInput, parse_macro_input};
 
+mod derive;
+
 #[proc_macro_derive(Message)]
-pub fn my_trait_derive(input: TokenStream) -> TokenStream {
+pub fn derive_message_trait(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let name = input.ident;
-
-    let expanded = quote! {
-        impl fairjax_core::Message for #name {}
-    };
-
-    TokenStream::from(expanded)
+    derive::derive_message_trait(&input).into()
 }
