0e123e301c5336601fd050dcba7425feb1fe9859
Emilostuff <emilostuff@gmail.com>
2025-12-08 20:38:18 +0100

    rebased to main head and added explixit lifetime

--- examples/examples/switch.rs
+++ examples/examples/switch.rs
@@ -15,25 +15,25 @@ pub enum Msg {
 
 use Msg::*;
 
-enum ActiveMailBox {
-    Mailbox0,
-    Mailbox1,
-    Mailbox2,
+enum ActiveMatcher {
+    Matcher0,
+    Matcher1,
+    Matcher2,
 }
 
-use ActiveMailBox::*;
+use ActiveMatcher::*;
 
-struct MailBoxManager<T> {
-    active_mailbox: RefCell<ActiveMailBox>,
+struct FairjaxManager<T> {
+    active_matcher: RefCell<ActiveMatcher>,
     mailboxes: [RefCell<MailBox<T>>; 3],
     queue: RefCell<VecDeque<T>>,
 }
 
-impl<T> MailBoxManager<T> {
+impl<T> FairjaxManager<T> {
     pub fn new() -> Self {
-        MailBoxManager {
-            active_mailbox: RefCell::new(ActiveMailBox::Mailbox0),
-            mailboxes: array::from_fn(|_| RefCell::new(MailBox::<T>::new())),
+        FairjaxManager {
+            active_matcher: RefCell::new(ActiveMatcher::Matcher0),
+            mailboxes: array::from_fn(|_| RefCell::new(MailBox::<T>::default())),
             queue: RefCell::new(VecDeque::new()),
         }
     }
@@ -46,60 +46,60 @@ impl<T> MailBoxManager<T> {
         self.queue.borrow_mut().pop_front()
     }
 
-    pub fn active_mailbox(&self) -> (ActiveMailBox, RefMut<MailBox<T>>) {
-        match *self.active_mailbox.borrow() {
-            Mailbox0 => (Mailbox0, self.mailboxes[0].borrow_mut()),
-            Mailbox1 => (Mailbox1, self.mailboxes[1].borrow_mut()),
-            Mailbox2 => (Mailbox2, self.mailboxes[2].borrow_mut()),
+    pub fn active_matcher<'a>(&'a self) -> (ActiveMatcher, RefMut<'a, MailBox<T>>) {
+        match *self.active_matcher.borrow() {
+            Matcher0 => (Matcher0, self.mailboxes[0].borrow_mut()),
+            Matcher1 => (Matcher1, self.mailboxes[1].borrow_mut()),
+            Matcher2 => (Matcher2, self.mailboxes[2].borrow_mut()),
         }
     }
 
-    pub fn switch_to(&self, target: ActiveMailBox, mut current: RefMut<MailBox<T>>) {
+    pub fn switch_to(&self, target: ActiveMatcher, mut current: RefMut<MailBox<T>>) {
         // Extract messages and push to processing queue
         let previous_messages = current.extract();
         for m in previous_messages.into_iter().rev() {
             self.queue.borrow_mut().push_front(m);
         }
 
-        // Set active mailbox:
-        *self.active_mailbox.borrow_mut() = target;
+        // Set active matcher:
+        *self.active_matcher.borrow_mut() = target;
     }
 }
 
 fn main() {
     let messages = vec![A, B, A, B, A, B, C];
-    let manager = MailBoxManager::new();
+    let manager = FairjaxManager::new();
 
     for incoming_msg in messages {
         manager.process_incoming(incoming_msg);
 
         // Extract from inner queue
         while let Some(msg) = manager.next() {
-            match manager.active_mailbox() {
-                (Mailbox0, mut mailbox) => {
+            match manager.active_matcher() {
+                (Matcher0, mut mailbox) => {
                     fairjax!(match msg >> [mailbox, Msg] {
                         C => {
                             println!("Received C, switch to inbox 1");
 
-                            manager.switch_to(Mailbox1, mailbox);
+                            manager.switch_to(Matcher1, mailbox);
                         }
                     });
                 }
-                (Mailbox1, mut mailbox) => {
+                (Matcher1, mut mailbox) => {
                     fairjax!(match msg >> [mailbox, Msg] {
                         A => {
                             println!("Received A, switch to inbox 2");
 
-                            manager.switch_to(Mailbox2, mailbox);
+                            manager.switch_to(Matcher2, mailbox);
                         }
                     });
                 }
-                (Mailbox2, mut mailbox) => {
+                (Matcher2, mut mailbox) => {
                     fairjax!(match msg >> [mailbox, Msg] {
                         B => {
                             println!("Received B, switch to inbox 1");
 
-                            manager.switch_to(Mailbox1, mailbox);
+                            manager.switch_to(Matcher1, mailbox);
                         }
                     });
                 }
